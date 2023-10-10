38628c71f3e1d65f0faaea89444e7446e785a106
p22p97 <pallvisingh221997@gmail.com>
2023-10-11 02:39:58 +0530

    Frontend code

--- Routes/login.js
+++ Routes/login.js
@@ -1,10 +1,10 @@
 const { signup, login } = require("../Backend/login.js")
 const express = require("express")
-const { generatetoken } = require("../middleware.js")
+const { generatetoken,generatetoken1 } = require("../middleware.js")
 
 const route = express.Router()
 
-route.post('/signup', generatetoken, signup) //using middleware to generate token
+route.post('/signup', generatetoken1, signup) //using middleware to generate token
 route.get('/login', generatetoken, login)
 
 module.exports = route
